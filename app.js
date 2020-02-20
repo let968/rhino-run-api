4b9d004d17239bc0dabfa68d0451792ecbc6dd03
Christian Letourneau <let968@gmail.com>
2020-02-20 11:13:44 -0500

    Remove restriction

--- app.js
+++ app.js
@@ -9,10 +9,8 @@ var setupController = require("./controllers/setup-controller");
 var allowCrossDomain = function(req, res, next) {
     var allowedOrigins = ['http://127.0.0.1:8080', 'https://rhino-run.letourneau.io'];
     var origin = req.headers.origin;
-    if(allowedOrigins.indexOf(origin) > -1){
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    
+
+    res.setHeader('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Methods', 'GET, POST');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', true);
